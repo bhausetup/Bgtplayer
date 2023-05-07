8b00061144606b753f7bcc4b9e0ebb61867c00a1
DOREMON <109501363+bhausetup@users.noreply.github.com>
2023-05-08 00:58:34 +0530

    Update start.py

--- plugins/bot/start.py
+++ plugins/bot/start.py
@@ -142,7 +142,7 @@ async def start_comm(client, message: Message, _):
                 )
             return
         if name[0:3] == "inf":
-            m = await message.reply_text("🔎")
+            m = await message.reply_text("😈")
             query = (str(name)).replace("info_", "", 1)
             query = f"https://www.youtube.com/watch?v={query}"
             results = VideosSearch(query, limit=1)
